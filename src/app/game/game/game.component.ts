867df0344516b1c8543e8fceb81ac6b3d96f8ac7
krith354 <cristian.ruiz1@upr.edu>
2020-05-17 12:12:00 -0400

    added fixes to jump in game section

--- src/app/game/game/game.component.ts
+++ src/app/game/game/game.component.ts
@@ -18,7 +18,8 @@ export class GameComponent implements OnInit {
   dx = 2;
   dy = -2;
   ballRadius = 10;
-  jumping: boolean = true;
+  jumping = true;
+  isJumping = false;
   myObstacles = [];
   myGamePiece;
   width;
@@ -43,82 +44,41 @@ export class GameComponent implements OnInit {
     vy: 9,
     color: 125
   }];
+  requestId3: number;
 
   constructor(public ngZone: NgZone, public ss: ShareStateService) {
-    window.addEventListener('keydown', function (e) {
-      if (e.keyCode == 32) {
-        console.log('test');
-        
-      }
-    });
     this.speedX = 0;
     this.speedY = 0;
     this.frameNo = 0;
-    // this.ngZone.runOutsideAngular(() => this.jump());
   }
 
   ngOnInit(): void {
     this.context = this.canvas.nativeElement.getContext('2d');
     this.x = 40;
     this.y = this.context.canvas.height - this.ballRadius - 0.01;
-    this.myGamePiece = new component(this.context, 20, 20, "#0095DD", 10, this.context.canvas.height-20);
-    // setInterval(() => {
-    //   this.updateGameArea();
-    // }, 3);
-    this.requestId2 = window.requestAnimationFrame(() => this.updateGameArea());
+    this.myGamePiece = new Block(this.context, 20, 20, '#0095DD', 10, this.context.canvas.height - 20);
+    this.requestId3 = window.requestAnimationFrame(() => this.updateGameArea());
+    if (!this.isJumping) {
+      document.body.onkeyup = (e) => {
+        if (e.which === 32) {
+          this.jump();
+        }
+      };
+    }
   }
 
   reset() {
+    window.cancelAnimationFrame(this.requestId3);
     this.context = this.canvas.nativeElement.getContext('2d');
-    this.myGamePiece = new component(this.context, 20, 20, "#0095DD", 10, this.context.canvas.height-20);
+    this.myGamePiece = new Block(this.context, 20, 20, '#0095DD', 10, this.context.canvas.height - 20);
     this.myObstacles = [];
     this.score = 0;
     this.lost = false;
+    this.ngOnInit();
   }
 
   exit() {
     this.ss.toggleGame();
-  }
-
-  animate() {
-    //draw the container
-    // this.context.fillStyle = "#000000";
-    // this.context.fillRect(this.container.x, this.container.y, this.container.width, this.container.height);
-  
-    //loop throughj the circles array
-    for (var i = 0; i < this.circles.length; i++) {
-      //draw the circles
-      this.clear();
-      this.context.fillStyle = 'hsl(' + this.circles[i].color++ + ', 100%, 50%)';
-      this.context.beginPath();
-      this.context.arc(this.circles[i].x, this.circles[i].y, this.circles[i].r, 0, Math.PI * 2, true);
-      this.context.fill();
-      this.context.closePath();
-  
-      //time to animate our circles ladies and gentlemen.
-      if (this.circles[i].x - this.circles[i].r + this.circles[i].vx < this.container.x || this.circles[i].x + this.circles[i].r + this.circles[i].vx > this.container.x + this.container.width) {
-        this.circles[i].vx = -this.circles[i].vx;
-      }
-  
-      if (this.circles[i].y + this.circles[i].r + this.circles[i].vy > this.container.y + this.container.height || this.circles[i].y - this.circles[i].r + this.circles[i].vy < this.container.y + 380) {
-        this.circles[i].vy = -this.circles[i].vy;
-      }
-  
-      // circles[i].x += circles[i].vx
-      this.circles[i].y += this.circles[i].vy;
-      
-    }
-    this.requestId = window.requestAnimationFrame(() => this.animate());
-  }
-
-  start() {
-    // this.jump();
-    this.requestId = window.requestAnimationFrame(() => this.jump());
-    // window.requestAnimationFrame(() => this.animate());
-    // this.interval = setInterval(() => {
-    //   this.jump();
-    //   this.requestId = requestAnimationFrame(() => this.start);
-    // }, 7);
   }
 
   stop() {
@@ -130,36 +90,29 @@ export class GameComponent implements OnInit {
   }
 
   jump() {
-    this.clear();
+    this.isJumping = true;
     if (this.myGamePiece.y < 400) {
       this.jumping = !this.jumping;
-    } else if (this.myGamePiece.y >= this.context.canvas.height-19) {
+    } else if (this.myGamePiece.y >= this.context.canvas.height - 19) {
       this.jumping = !this.jumping;
     }
     if (this.jumping) {
-      this.myGamePiece.y = this.myGamePiece.y - 7;
+      this.myGamePiece.y = this.myGamePiece.y - 3;
       this.myGamePiece.updateJump(this.myGamePiece.x, this.myGamePiece.y);
     } else {
-      this.myGamePiece.y = this.myGamePiece.y + 7;
+      this.myGamePiece.y = this.myGamePiece.y + 3;
       this.myGamePiece.updateJump(this.myGamePiece.x, this.myGamePiece.y);
     }
     this.requestId = window.requestAnimationFrame(() => this.jump());
     setTimeout(() => {
       window.cancelAnimationFrame(this.requestId);
-    }, 400);
-  }
-
-  drawBall() {
-    this.context.beginPath();
-    this.context.arc(this.x, this.y, this.ballRadius, 0, Math.PI*2);
-    this.context.fillStyle = 'hsl(' + this.circles[0].color++ + ', 100%, 50%)';
-    this.context.fill();
-    this.context.closePath();
+      this.isJumping = false;
+    }, 378);
   }
 
   updateGameArea() {
-    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (var i = 0; i < this.myObstacles.length; i += 1) {
+    let x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (let i = 0; i < this.myObstacles.length; i += 1) {
       if (this.myGamePiece.crashWith(this.myObstacles[i])) {
         this.stop();
         this.lost = true;
@@ -168,40 +121,36 @@ export class GameComponent implements OnInit {
     }
     this.clear();
     this.frameNo += 1;
-    if (this.frameNo == 1 || this.everyInterval(200)) {
+    if (this.frameNo === 1 || this.everyInterval(200)) {
         x = this.context.canvas.width;
         minHeight = 450;
         maxHeight = 490;
-        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
         minGap = 3;
         maxGap = 3;
-        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        this.myObstacles.push(new component(this.context, 10, x - height - gap, "green", x - 50, height + gap));
+        gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+        this.myObstacles.push(new Block(this.context, 10, x - height - gap, 'green', x - 50, height + gap));
     }
-    for (var i = 0; i < this.myObstacles.length; i += 1) {
+    for (let i = 0; i < this.myObstacles.length; i += 1) {
       this.myObstacles[i].x += -4;
-      if (this.myObstacles[i].x == this.myGamePiece.x) {
+      if (this.myObstacles[i].x === this.myGamePiece.x) {
         this.score = this.score + 5;
       }
       this.myObstacles[i].update();
     }
-    this.myGamePiece.newPos();    
+    this.myGamePiece.newPos();
     this.myGamePiece.update();
-    this.requestId2 = window.requestAnimationFrame(() => this.updateGameArea());
+    this.requestId3 = window.requestAnimationFrame(() => this.updateGameArea());
   }
+
   everyInterval(n) {
-    if ((this.frameNo / n) % 1 == 0) {
+    if ((this.frameNo / n) % 1 === 0) {
       return true;
     }
     return false;
   }
-
-  ngOnDestroy() {
-    clearInterval(this.interval);
-    cancelAnimationFrame(this.requestId);
-  }
 }
-class component {
+class Block {
   width;
   height;
   speedX;
@@ -215,12 +164,12 @@ class component {
     this.width = width;
     this.height = height;
     this.speedX = 0;
-    this.speedY = 0;    
+    this.speedY = 0;
     this.x = x;
     this.y = y;
     this.ctx = ctx;
     this.color = color;
-  }  
+  }
 
   public update() {
     this.ctx.fillStyle = this.color;
@@ -232,18 +181,18 @@ class component {
   }
   public newPos() {
     this.x += this.speedX;
-    this.y += this.speedY;        
-  }    
+    this.y += this.speedY;
+  }
   public crashWith(otherobj) {
-    var myleft = this.x;
-    var myright = this.x + (this.width);
-    var mytop = this.y;
-    var mybottom = this.y + (this.height);
-    var otherleft = otherobj.x;
-    var otherright = otherobj.x + (otherobj.width);
-    var othertop = otherobj.y;
-    var otherbottom = otherobj.y + (otherobj.height);
-    var crash = true;
+    const myleft = this.x;
+    const myright = this.x + (this.width);
+    const mytop = this.y;
+    const mybottom = this.y + (this.height);
+    const otherleft = otherobj.x;
+    const otherright = otherobj.x + (otherobj.width);
+    const othertop = otherobj.y;
+    const otherbottom = otherobj.y + (otherobj.height);
+    let crash = true;
     if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
         crash = false;
     }
