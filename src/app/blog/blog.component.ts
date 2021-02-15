cf06bf05cb68160817a2d18f8b565c73b68b2c8a
krith354 <cristian.ruiz1@upr.edu>
2021-02-15 14:04:54 -0400

    adding password to capstone blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -10,9 +10,16 @@ export class BlogComponent implements OnInit {
 
   constructor(private titleService: Title) { }
   newTitle = 'Coki Mu Solutions Blog';
+  pass = 'capstone2021';
 
   ngOnInit(): void {
     this.setTitle(this.newTitle);
+    const password = prompt('Please enter password to see this site: ', '');
+    if (password === this.pass) {
+      alert('Correct. Please press OK to enter.');
+    } else {
+      window.location.reload();
+    }
   }
 
   public setTitle(newTitle: string) {
